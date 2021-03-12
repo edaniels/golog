5f44053d1c35cb6efb3cced015b5dbd81710c6d5
Eric Daniels <eric@erdaniels.com>
2021-03-12 11:34:46 -0500

    add test utils

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/edaniels/golog
 
 go 1.15
 
-require go.uber.org/zap v1.16.0
+require (
+	github.com/edaniels/golinters v0.0.4
+	github.com/golangci/golangci-lint v1.38.0
+	go.uber.org/zap v1.16.0
+)
--- logger.go
+++ logger.go
@@ -1,6 +1,13 @@
 package golog
 
-import "go.uber.org/zap"
+import (
+	"testing"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest"
+	"go.uber.org/zap/zaptest/observer"
+)
 
 type Logger = *zap.SugaredLogger
 
@@ -12,4 +19,20 @@ func init() {
 		panic(err)
 	}
 	Global = logger.Sugar()
+}
+
+// NewTestLogger directs logs to the go test logger.
+func NewTestLogger(t *testing.T) Logger {
+	logger, _ := NewObservedTestLogger(t)
+	return logger
+}
+
+// NewObservedTestLogger is like NewTestLogger but also saves logs to an in memory observer.
+func NewObservedTestLogger(t *testing.T) (Logger, *observer.ObservedLogs) {
+	logger := zaptest.NewLogger(t)
+	observerCore, observedLogs := observer.New(zap.LevelEnablerFunc(zapcore.DebugLevel.Enabled))
+	logger = logger.WithOptions(zap.WrapCore(func(c zapcore.Core) zapcore.Core {
+		return zapcore.NewTee(c, observerCore)
+	}))
+	return logger.Sugar(), observedLogs
 }
