87c6c1635855953234793ec732dcbbd27c8b2b87
Eric Daniels <eric@erdaniels.com>
2021-03-26 13:34:25 -0400

    borrow configs from zap/config.go

--- logger.go
+++ logger.go
@@ -14,11 +14,75 @@ type Logger = *zap.SugaredLogger
 var Global Logger
 
 func init() {
-	logger, err := zap.NewDevelopment()
+	logger, err := defaultDevelopmentConfig.Build()
 	if err != nil {
 		panic(err)
 	}
 	Global = logger.Sugar()
+}
+
+// from https://github.com/uber-go/zap/blob/2314926ec34c23ee21f3dd4399438469668f8097/config.go#L98
+// but disable stacktraces.
+var defaultProductionConfig = zap.Config{
+	Encoding: "json",
+	EncoderConfig: zapcore.EncoderConfig{
+		TimeKey:        "ts",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		FunctionKey:    zapcore.OmitKey,
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.EpochTimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	},
+	DisableStacktrace: true,
+	OutputPaths:       []string{"stdout"},
+	ErrorOutputPaths:  []string{"stderr"},
+}
+
+// from https://github.com/uber-go/zap/blob/2314926ec34c23ee21f3dd4399438469668f8097/config.go#L135
+// but disable stacktraces, use same keys as prod, and color levels.
+var defaultDevelopmentConfig = zap.Config{
+	Encoding: "console",
+	EncoderConfig: zapcore.EncoderConfig{
+		TimeKey:        "ts",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		FunctionKey:    zapcore.OmitKey,
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalColorLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	},
+	DisableStacktrace: true,
+	OutputPaths:       []string{"stdout"},
+	ErrorOutputPaths:  []string{"stderr"},
+}
+
+// NewLogger returns a new logger using the default production configuration.
+func NewLogger(name string) Logger {
+	logger, err := defaultProductionConfig.Build()
+	if err != nil {
+		Global.Fatal(err)
+	}
+	return logger.Sugar().Named(name)
+}
+
+// NewDevelopmentLogger returns a new logger using the default development configuration.
+func NewDevelopmentLogger(name string) Logger {
+	logger, err := defaultDevelopmentConfig.Build()
+	if err != nil {
+		Global.Fatal(err)
+	}
+	return logger.Sugar().Named(name)
 }
 
 // NewTestLogger directs logs to the go test logger.
